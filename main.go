47d1fff30903668777ebe518c4647dd1273931f8
Chaz Gregory <speshled@gmail.com>
2023-07-23 13:55:45 -0500

    try new range

--- main.go
+++ main.go
@@ -19,10 +19,12 @@ const servoPinID = 13
 // const tiltPinID = 15
 
 const frequency = 100000
-const cycleLen = uint32(255)
-const maxvalue = uint32(200)
-const midvalue = uint32(150)
-const minvalue = uint32(100)
+const cycleLen = uint32(1000)
+const maxvalue = uint32(100)
+const midvalue = uint32(75)
+const minvalue = uint32(50)
+
+const enableServo = true
 
 func main() {
 	err := rpio.Open()
@@ -31,24 +33,32 @@ func main() {
 	}
 	defer rpio.Close()
 
-	servo := rpio.Pin(servoPinID)
-	servo.Mode(rpio.Pwm)
-	//c.pins.servo.Pwm()
-	servo.Freq(frequency)
+	if enableServo {
+		servo := rpio.Pin(servoPinID)
+		servo.Mode(rpio.Pwm)
+		//c.pins.servo.Pwm()
+		servo.Freq(frequency)
 
-	servo.DutyCycle(midvalue, cycleLen)
-	log.Printf("Servo to middle")
-	time.Sleep(5 * time.Second)
+		servo.DutyCycle(midvalue, cycleLen)
+		log.Printf("Servo to middle")
+		time.Sleep(5 * time.Second)
 
-	log.Println("Start sweeping")
-	i := uint32(100)
-	for {
-		if i > maxvalue {
-			i = minvalue
+		log.Println("Start sweeping")
+		i := uint32(100)
+		for {
+			if i > maxvalue {
+				i = minvalue
+			}
+			//log.Printf("Sending %d of %d\n", i, maxvalue)
+			servo.DutyCycle(i, cycleLen)
+			time.Sleep(20 * time.Millisecond)
+			i++
 		}
-		//log.Printf("Sending %d of %d\n", i, maxvalue)
-		servo.DutyCycle(i, cycleLen)
-		time.Sleep(20 * time.Millisecond)
-		i++
+	} else {
+		esc := rpio.Pin(servoPinID)
+		esc.Mode(rpio.Pwm)
+		//c.pins.servo.Pwm()
+		esc.Freq(frequency)
 	}
+
 }
